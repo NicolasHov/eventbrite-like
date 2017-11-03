5eb9bce3e1d86563f630ac0925f721c98790af50
NicolasHov <kola.hov@gmail.com>
2017-11-03 19:21:53 +0100

    WIP Generate controller User

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def index
-    puts "Hello World !"
+  def hello
+    render html: "hello guys"
   end
 end
